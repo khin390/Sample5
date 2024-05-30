893a7cb5dc50a76d6aa745f60bbc3920a90ccd39
khin390 <khinyadanarsoe239@gmail.com>
2024-05-30 15:24:53 +0630

    upload sample5

---
+++ library/admin.py
@@ -0,0 +1,7 @@
+# myapp/admin.py
+
+from django.contrib import admin
+from .models import Author, Book
+
+admin.site.register(Author)
+admin.site.register(Book)
---
+++ library/urls.py
@@ -0,0 +1,8 @@
+# myapp/urls.py
+
+from django.urls import path
+from .views import book_list
+
+urlpatterns = [
+    path('books/', book_list, name='book_list'),
+]
